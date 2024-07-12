f8e73cb7844b336a32e543ff743624bcd4b65abb
Cae <gemwright@proton.me>
2024-07-12 19:13:39 +1200

    reformat display links

--- client/components/DisplayLinks.tsx
+++ client/components/DisplayLinks.tsx
@@ -1,6 +1,18 @@
-function DisplayLinks(props : {completedSet : string[]}){
-    const {completedSet} = props
-    return <div className="grid grid-cols-1 gap-4 w-full text-center mb-4">{completedSet.map((item, index) => <div className="bg-white text-black py-2 px-4 rounded" key={index}><p>{item}</p></div>)}</div>
+import React from 'react'
+interface DisplayLinksProps {
+  completedSet: string[]
 }
+
+const DisplayLinks: React.FC<DisplayLinksProps> = ({ completedSet }) => {
+    return (
+        <div className="grid grid-cols-1 gap-4 w-full text-center mb-4">
+            {completedSet.map((item) => 
+            <div className="bg-white text-black py-2 px-4 rounded" key={item}>
+                <p>{item}</p>
+            </div>)}
+        </div>
+    )
+  }
+
 
 export default DisplayLinks
